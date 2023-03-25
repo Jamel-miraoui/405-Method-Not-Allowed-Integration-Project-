77718ce37a6d6e1bed5de6c5e8e8125ca1e2c379
ahmed-oues <ahmed.oueslati6110@gmail.com>
2023-03-25 19:07:38 +0100

    i think this is better
    hedi 5dmett zbii yodherlyy 5iir, choufou rwe7kom w 9Oloui chnwa namlou

---
+++ ahmed attampt/books.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Books | Library</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li class="active"><a href="#">Books</a></li>
+					<li><a href="courses.php">Courses</a></li>
+					<li><a href="about.php">About</a></li>
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
+			</nav>
+			<h1>Library</h1>
+		</div>
+	</header>
+	<main>
+		<div class="container">
+			<h2>Books</h2>
+			<form action="#" method="GET">
+				<input type="text" name="search" placeholder="Search...">
+				<button type="submit">Go</button>
+			</form>
+			<div class="books">
+				<div class="book">
+					<img src="resorses/p3.jpeg" width="200">
+					<h3>The Catcher in the Rye</h3>
+					<p>by J.D. Salinger</p>
+					<p>Published by Little, Brown and Company</p>
+					<p>ISBN: 9780316769174</p>
+					<p>Publication Date: 07/16/1951</p>
+					<p>Number of Pages: 277</p>
+					<p>Category: Fiction</p>
+					<a href="#">Borrow</a>
+				</div>
+                <div class="books">
+				<div class="book">
+					<img src="resorses/p4.jpg"  width="200">
+					<h3>The Catcher in the Rye</h3>
+					<p>by J.D. Salinger</p>
+					<p>Published by Little, Brown and Company</p>
+					<p>ISBN: 9780316769174</p>
+					<p>Publication Date: 07/16/1951</p>
+					<p>Number of Pages: 277</p>
+					<p>Category: Fiction</p>
+					<a href="#">Borrow</a>
+				</div>
+				<div class="book">
+					<img src="resorses/p2.jpeg" width="200">
+					<h3>1984</h3>
+					<p>by George Orwell</p>
+					<p>Published by Harvill Secker</p>
+					<p>ISBN: 9781846553265</p>
+					<p>Publication Date: 06/08/1949</p>
+					<p>Number of Pages: 328</p>
+					<p>Category: Fiction</p>
+					<a href="#">Borrow</a>
+				</div>
+				<div class="book">
+					<img src="resorses/p1.jpeg" width="200">
+					<h3>To Kill a Mockingbird</h3>
+					<p>by Harper Lee</p>
+					<p>Published by J. B. Lippincott & Co.</p>
+					<p>ISBN: 9780061120084</p>
+					<p>Publication Date: 07/11/1960</p>
+					<p>Number of Pages: 281</p>
+					<p>Category: Fiction</p>
+					<a href="#">Borrow</a>
+				</div>
+				<!-- add more books here -->
+			</div>
+		</div>
+	</main>
+	<footer>
+		<div class="container">
+			<p>&copy; 2023 Library</p>
+		</div>
+	</footer>
+</body>
+</html>
