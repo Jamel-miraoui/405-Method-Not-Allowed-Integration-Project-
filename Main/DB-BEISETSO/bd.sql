91fb1a34ac2c4562f31620720e01b547bdd40540
ahmed-oues <ahmed.oueslati6110@gmail.com>
2023-03-26 17:26:40 +0100

    auto_increment added in data basse
    somthing requred to avoid coflicts wen passing informaton to data base trugh php
    admin user added

--- Main/DB-BEISETSO/bd.sql
+++ Main/DB-BEISETSO/bd.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Users (
-  user_id INT PRIMARY KEY,
+  user_id INT auto_increment PRIMARY KEY,
   name VARCHAR(255),
   email VARCHAR(255),
   address VARCHAR(255),
@@ -11,7 +11,7 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Moderators (
-  moderator_id INT PRIMARY KEY,
+  moderator_id INT auto_increment PRIMARY KEY,
   name VARCHAR(255),
   email VARCHAR(255),
   address VARCHAR(255),
@@ -20,7 +20,7 @@ CREATE TABLE Moderators (
 );
 
 CREATE TABLE Admins (
-  admin_id INT PRIMARY KEY,
+  admin_id INT auto_increment PRIMARY KEY,
   name VARCHAR(255),
   email VARCHAR(255),
   address VARCHAR(255),
@@ -29,7 +29,7 @@ CREATE TABLE Admins (
 );
 
 CREATE TABLE Books (
-  book_id INT PRIMARY KEY,
+  book_id INT auto_increment PRIMARY KEY,
   title VARCHAR(255),
   author_name VARCHAR(255),
   publisher_name VARCHAR(255),
@@ -41,21 +41,21 @@ CREATE TABLE Books (
 ALTER TABLE books ADD COLUMN pdf_file BLOB;
 
 CREATE TABLE Authors (
-  author_id INT PRIMARY KEY,
+  author_id INT auto_increment PRIMARY KEY,
   name VARCHAR(255),
   date_of_birth DATE,
   biography VARCHAR(255)
 );
 
 CREATE TABLE Publishers (
-  publisher_id INT PRIMARY KEY,
+  publisher_id INT auto_increment PRIMARY KEY,
   name VARCHAR(255),
   location VARCHAR(255),
   contact_information VARCHAR(255)
 );
 
 CREATE TABLE Borrow_history (
-  borrow_id INT PRIMARY KEY,
+  borrow_id INT auto_increment PRIMARY KEY,
   book_id INT,
   user_id INT,
   borrow_date DATE,
@@ -67,12 +67,12 @@ CREATE TABLE Borrow_history (
 );
 
 CREATE TABLE Categories (
-  category_id INT PRIMARY KEY,
+  category_id INT auto_increment PRIMARY KEY,
   name VARCHAR(255)
 );
 
 CREATE TABLE Reviews (
-  review_id INT PRIMARY KEY,
+  review_id INT auto_increment PRIMARY KEY,
   book_id INT,
   user_id INT,
   review_text VARCHAR(255),
@@ -82,7 +82,7 @@ CREATE TABLE Reviews (
 );
 
 CREATE TABLE Courses (
-  course_id INT PRIMARY KEY,
+  course_id INT auto_increment PRIMARY KEY,
   title VARCHAR(255),
   description VARCHAR(255),
   instructor_name VARCHAR(255),
@@ -94,7 +94,7 @@ CREATE TABLE Courses (
 );
 
 CREATE TABLE Course_files (
-  file_id INT PRIMARY KEY,
+  file_id INT auto_increment PRIMARY KEY,
   course_id INT,
   file_name VARCHAR(255),
   file_type VARCHAR(255),
@@ -103,13 +103,13 @@ CREATE TABLE Course_files (
 );
 
 CREATE TABLE Departments (
-  department_id INT PRIMARY KEY,
+  department_id INT auto_increment PRIMARY KEY,
   name VARCHAR(255),
   description VARCHAR(255)
 );
 
 CREATE TABLE Faculties (
-  faculty_id INT PRIMARY KEY,
+  faculty_id INT auto_increment PRIMARY KEY,
   name VARCHAR(255),
   email VARCHAR(255),
   department_id INT,
@@ -118,10 +118,16 @@ CREATE TABLE Faculties (
 );
 
 CREATE TABLE Students (
-  student_id INT PRIMARY KEY,
+  student_id INT auto_increment PRIMARY KEY,
   name VARCHAR(255),
   email VARCHAR(255),
   address VARCHAR(255),
   enrollment_date DATE,
   graduation_date DATE
 );
+
+ //adinding admin account
+ 
+INSERT INTO Users (user_id, name, email, address, username, password, user_type, is_moderator, is_admin)
+VALUES (1, 'ahmed', 'ahmed.oueslati6110@gmail.com', 'gaafour', 'admin', 'admin123', 'admin', 0, 1);
+
