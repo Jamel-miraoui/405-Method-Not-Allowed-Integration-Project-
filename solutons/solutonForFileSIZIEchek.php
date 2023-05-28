a4b259c9d9fa35addc0db9a614b88477aabeabf7
ahmed-oues <ahmed.oueslati6110@gmail.com>
2023-05-28 21:33:05 +0100

    Semi final commit

--- solutons/solutonForFileSIZIEchek.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_FILES['file'])) {
-    if($_FILES['file']['size'] > 10485760) { //10 MB (size is also in bytes)
-        // File too big
-    } else {
-        // File within size restrictions
-    }
-}
